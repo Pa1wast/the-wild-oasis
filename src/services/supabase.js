c72752a179531356889f833e7bbf4fd819d622d0
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2025-01-11 19:03:36 +0300

    Update supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://eikzgovtnjzwrjzuvith.supabase.co';
+export const supabaseUrl = 'https://hbltivphcyncyavtwyvk.supabase.co';
 const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpa3pnb3Z0bmp6d3JqenV2aXRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcxNDg2NzcsImV4cCI6MjAyMjcyNDY3N30.XF3Qpt1PaZBk5A2irqtXI2PmxJNG6-WGqb39VxvnkxA';
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhibHRpdnBoY3luY3lhdnR3eXZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2MTAwNDksImV4cCI6MjA1MjE4NjA0OX0.DwSAoER8fHfD0agv9YGyE9z-kQWne2Dou9cVly1j1oI';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
